1c5024384866ebbd93b7ddc4d8f9cfb69e4bf150
jfuent115 <jfuent115@gmail.com>
2014-09-18 23:13:09 -0500

    Added in some php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$varOne = 10;
+$varTwo = 15;
+$answer = $varOne + $varTwo;
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>First Website</title>
+	</head>
+	<body>
+		<h1>Hello World</h1>
+		<p>This is our paragraph</p>
+		<?php echo $answer; ?>
+	</body>
+</html>
